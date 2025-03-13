be995f31a9ae961bfd491db51cf864d137748965
s <marcin.kacprowicz@p.lodz.pl>
2025-03-13 09:12:07 +0100

    Komit iniccccjalizacyjny - po pierwszym wykladzie

---
+++ WykladVS2022/WykladVS2022/Controllers/PersonsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WykladVS2022.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WykladVS2022.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return new Person[] {
+                new Person { Id = 1, Name = "Jan", Surname= "Kowalski" },
+                new Person { Id = 2, Name = "Stanisław", Surname = "Nowaka" }
+                };                      
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return value.ToString();
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
